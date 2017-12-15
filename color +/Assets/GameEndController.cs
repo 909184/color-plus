8b6db7aac690e8bb26d817bbeaf675f03e756ff5
MIHASHI\hime <alw17@hampshire.edu>
2017-12-15 17:04:05 -0500

    final

---
+++ color +/Assets/GameEndController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEndController : MonoBehaviour {
+	public Text textBox;
+
+	// Use this for initialization
+	void Start () {
+		textBox.text = ("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= GameController.gameTime) {
+			textBox.text = ("game over!" +
+			"score: " + GameController.score);
+		}
+	}
+}
+
